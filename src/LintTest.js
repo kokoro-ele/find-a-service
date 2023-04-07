06bfb698ce17266cadad63d20500c7ea2fb4ab64
honghui <hs5n22@soton.ac.uk>
2023-04-07 16:52:45 +0100

    test lint tool

--- src/LintTest.js
+++ src/LintTest.js
@@ -1,5 +1,11 @@
 function testFormatter() {
   let a = 1234
+  let str = 'this is a string another'
+  let str2 = 'this is a another'
+  console.log('hello this is')
+  let str3 = 'hello'
 }
 
-setTimeout(() => {}, 1000)
+setTimeout(() => {
+  console.log('hello')
+}, 1000)
