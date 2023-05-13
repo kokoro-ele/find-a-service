b6855dbc9fd69c6d68a36242604cca6115568528
kokoro-ele <kokoro_ele@163.com>
2023-05-13 12:36:20 +0100

    init

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
-import { auth } from '../utils/FirebaseSetup'
 import { useEffect, useState } from 'react'
-import { getAuth } from 'firebase/auth'
 import {
   addFakeData,
   getServiceProviderByApproved,
